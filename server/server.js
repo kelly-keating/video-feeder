d3859caf8867764b88d536e45e503e00b7928032
Kelly Keating <keating.kel@gmail.com>
2020-09-12 19:55:15 +1200

    routes >> youtubeRoutes

--- server/server.js
+++ server/server.js
@@ -1,13 +1,13 @@
 const path = require('path')
 const express = require('express')
 
-const routes = require('./routes/youtube')
+const youtubeRoutes = require('./routes/youtube')
 
 const server = express()
 
 server.use(express.json())
 server.use(express.static(path.join(__dirname, 'public')))
 
-server.use('/v1/youtube', routes)
+server.use('/v1/youtube', youtubeRoutes)
 
 module.exports = server

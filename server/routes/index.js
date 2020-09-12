ea08d0874da826143e7bed7bf495f3c6498ce17e
Kelly Keating <keating.kel@gmail.com>
2020-09-12 20:57:25 +1200

    tidy up

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,14 +3,14 @@ const router = express.Router()
 
 const { getChannelFeed } = require('./youtubeApi')
 
-// add route
+// add a channel
   // verify channel id (on front end?)
   // add channel to db
   // get videos for that channel? or just refresh all?
 
-// get all 
+// get all videos
   // get all channels from db
   // for each do a request
   // after, map and combine
 
-  module.exports = router
+module.exports = router

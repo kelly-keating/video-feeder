cf6085c5408af9b5b5ef4ccf6810988bdfcadedf
Kelly Keating <keating.kel@gmail.com>
2020-09-21 10:47:08 +1200

    deploy

--- client/components/VideoList.jsx
+++ client/components/VideoList.jsx
@@ -1,28 +1,35 @@
+import moment from 'moment'
 import React from 'react'
 import { connect } from 'react-redux'
 
 
 class VideoList extends React.Component {
 
-  renderVideos() {
+  renderCard = (video) => {
+    const published = moment(video.published)
+
     return (
-      <div className="tile is-ancestor">
-        {this.props.videos.map(video => (
-          <div key={video.id} className="tile is-parent is-3">
-            <div className="card">
-              <div className="card-image">
-                <img src={video.thumbnail}  className="is-3-tablet" alt="Placeholder image" />
-              </div>
-              <div className="card-content">
-                <p className="title is-5">{video.title}</p>
-                <p className="subtitle is-6">{new Date(video.published).toLocaleString()}</p>
-                <div className="content">
-                  {video.description.split('\n')[0]}
-                </div>
-              </div>
+      <div key={video.id} className="tile is-parent is-3">
+        <div className="card">
+          <div className="card-image">
+            <img src={video.thumbnail}  className="is-3-tablet" alt="Placeholder image" />
+          </div>
+          <div className="card-content">
+            <p className="title is-5">{video.title}</p>
+            <p className="subtitle is-6">{published.format('ddd DD MMM')}</p>
+            <div className="content">
+              {video.description.split('\n')[0]}
             </div>
           </div>
-        ))}
+        </div>
+      </div>
+    )
+  }
+
+  renderVideos = () => {
+    return (
+      <div className="tile is-ancestor">
+        {this.props.videos.map(video => this.renderCard(video))}
       </div>
     )
   }

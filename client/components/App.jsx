5fddb44d1c5a9f808fa89463ee3b68a932443b6b
Kelly Keating <keating.kel@gmail.com>
2021-05-26 16:38:34 +1200

    take it back

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -12,36 +12,35 @@ import { saveAllTheData, addVideos, updateVideos } from '../actions'
 
 function App () {
   useEffect(() => {
-    // getChannelInfo('UC-7oMv6E4Uz2tF51w5Sj49w')
+    getChannelInfo('UC-7oMv6E4Uz2tF51w5Sj49w')
+      .then(console.log)
 
-    getAllData()
-      .then(data => {
-        this.props.dispatch(saveAllTheData(data))      
-        this.refreshVideos()
-      })
-      .catch(err => console.log(err.message))
+    // getAllData()
+    //   .then(data => {
+    //     this.props.dispatch(saveAllTheData(data))      
+    //     this.refreshVideos()
+    //   })
+    //   .catch(err => console.log(err.message))
   }, [])     
 
-  const refreshVideos = () => {
-    refreshFeeds()
-      .then(videos => {
-        this.props.dispatch(addVideos(videos.new))
-        this.props.dispatch(updateVideos(videos.updated))
-      })
-      .catch(err => console.log(err.message))
-  }
+  // const refreshVideos = () => {
+  //   refreshFeeds()
+  //     .then(videos => {
+  //       this.props.dispatch(addVideos(videos.new))
+  //       this.props.dispatch(updateVideos(videos.updated))
+  //     })
+  //     .catch(err => console.log(err.message))
+  // }
 
   return (
     <div className="container" >
       <h1>Title - Hi :)</h1>
       <Nav />
-      {/* 
-      <AddFeed />
+      {/* <AddFeed /> */}
       <Switch>
-        <Route path='/subs' component={SubscriptionList} />
+        {/* <Route path='/subs' component={SubscriptionList} /> */}
         <Route path='/' component={VideoList} />
       </Switch> 
-      */}
     </div>
   )
 }

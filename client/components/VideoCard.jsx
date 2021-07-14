37e97ab35d34d4f44a35ca6e6855ae5101666dd7
Kelly Keating <keating.kel@gmail.com>
2021-07-14 17:37:55 +1200

    don't break if video messed up in db

--- client/components/VideoCard.jsx
+++ client/components/VideoCard.jsx
@@ -7,7 +7,7 @@ import { deleteVideo } from '../api/index'
 
 function VideoCard ({ video, dispatch }) {
   const published = moment(video.published)
-  const blurb = video.description.split('↵')[0]
+  const blurb = video.description?.split('↵')[0]
 
   const deleteVid = () => {
     const {id} = video
@@ -19,11 +19,11 @@ function VideoCard ({ video, dispatch }) {
     <div key={video.id} className="tile is-parent is-3">
       <div className="card">
         <div className="card-image">
-          <img src={video.thumbnails.medium.url} className="is-3-tablet" alt="Placeholder image" />
+          <img src={video.thumbnails?.medium.url} className="is-3-tablet" alt="Placeholder image" />
         </div>
         <div className="card-content">
           <p className="title is-5">{video.title}</p>
-          <p className="subtitle is-6">{published.format('ddd DD MMM')}</p>
+          <p className="subtitle is-6">{published?.format('ddd DD MMM')}</p>
           <div className="content">
             {blurb}
           </div>
